8e6b10d71a4c6d07373ccafe3124d218c72e8702
Curtis Shelton <ccshelton@gmail.com>
2017-01-19 22:49:04 -0600

    Scope users to subdomain, update validations accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,27 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
+
+  validates_uniqueness_to_tenant :email
+  validates_presence_of   :email, if: :email_required?
+
+  validates_presence_of     :password, if: :password_required?
+  validates_confirmation_of :password, if: :password_required?
+
+  def self.find_for_authentication(warden_conditions)
+    where(:email => warden_conditions[:email], :subdomain => warden_conditions[:subdomain]).first
+  end
+
+  protected
+    # Checks whether a password is needed or not. For validations only.
+    # Passwords are always required if it's a new record, or if the password
+    # or confirmation are being set somewhere.
+    def password_required?
+      !persisted? || !password.nil? || !password_confirmation.nil?
+    end
+
+    def email_required?
+      true
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118034202) do
+ActiveRecord::Schema.define(version: 20170120042212) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -53,9 +53,10 @@ ActiveRecord::Schema.define(version: 20170118034202) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "app_id"
+    t.string   "subdomain"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email", "subdomain"], name: "index_users_on_email_and_subdomain", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
