bbe3df4885914e95d7caf20ff3e52820035f7df9
Curtis Shelton <ccshelton@gmail.com>
2017-01-22 15:26:49 -0600

    Include user_id if enabled in intercomSettings, build intercomSettings in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,46 @@ module ApplicationHelper
     when 'alert' then "alert alert-warning"
     end
   end
+
+  def intercom_settings
+    if current_tenant
+      if current_user
+        logged_in_js(current_user)
+      elsif current_admin
+        logged_in_js(current_admin)
+      else
+        logged_out_js
+      end
+    end
+  end
+
+  def logged_in_js(user)
+    javascript_tag("window.intercomSettings = {#{get_custom_settings} app_id: '#{current_tenant.intercom_id}', email: '#{user.email}', created_at: #{user.created_at.to_i}};")
+  end
+
+  def logged_out_js
+    javascript_tag("window.intercomSettings = {app_id: '#{current_tenant.intercom_id}'};")
+  end
+
+  def get_custom_settings
+    settings = ""
+    settings += get_user_id.to_s
+    settings
+  end
+
+  def get_user_id
+    if user_id_enabled?
+      if current_user
+        "user_id: 'user_#{current_user.id}',"
+      elsif current_admin
+        "user_id: 'admin_#{current_admin.id}',"
+      end
+    end
+  end
+
+  def user_id_enabled?
+    if current_tenant.user_id_enabled?
+      true
+    end
+  end
 end
