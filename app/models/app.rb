83330fb66a52ba2053ea2268e64f65e006514a3a
Slonski Andrey <andrey1@C02SK1LLFVH6.intercom.io>
2017-05-17 17:54:47 -0500

    Add lowercase validation to subdomain name

--- app/models/app.rb
+++ app/models/app.rb
@@ -5,5 +5,6 @@ class App < ActiveRecord::Base
   validates :subdomain, uniqueness: true
   validates :subdomain, :intercom_id, format: { with: /\A[a-zA-Z0-9]*\z/, message: ": only letters and/or numbers are allowed"}
   validates :subdomain, length: {maximum: 20, too_long: ": %{count} characters is the maximum allowed"}
+  validates :subdomain, format: { with: /\A[a-z0-9]+\z/ }
   validates :intercom_id, length: {maximum: 50, too_long: ": %{count} characters is the maximum allowed"}
 end
