202d038ae407229c8e02357f4ea29fa29965ba26
Curtis Shelton <curtisshelton@Curtiss-MacBook-Pro-2.local>
2017-05-22 16:54:18 -0500

    Validate uniqueness of intercom_id and scope that to admins

--- app/models/app.rb
+++ app/models/app.rb
@@ -6,4 +6,5 @@ class App < ActiveRecord::Base
   validates :subdomain, :intercom_id, format: { with: /\A[a-zA-Z0-9]*\z/, message: ": only letters and/or numbers are allowed"}
   validates :subdomain, length: {maximum: 20, too_long: ": %{count} characters is the maximum allowed"}
   validates :intercom_id, length: {maximum: 50, too_long: ": %{count} characters is the maximum allowed"}
+  validates :intercom_id, :uniqueness => {:scope => :admin_id, message: ": You already have a site with that Intercom app id"}
 end
