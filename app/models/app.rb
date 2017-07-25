76f280e6edd3b4929ca7dcae25d81afffd945c68
Curtis Shelton <curtisshelton@Curtiss-MacBook-Pro-2.local>
2017-07-25 14:51:18 -0500

    Fix validation on subdomain

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,7 +3,7 @@ class App < ActiveRecord::Base
 
   validates :subdomain, :admin_id, :intercom_id, presence: true
   validates :subdomain, uniqueness: true
-  validates :subdomain, :intercom_id, format: { with: /\A[a-zA-Z0-9]*\z/, message: ": only letters and/or numbers are allowed"}
+  validates :subdomain, :intercom_id, format: { with: /\A[a-z0-9]*\z/, message: ": must use lower-case letter or numbers, without spaces."}
   validates :subdomain, length: {maximum: 20, too_long: ": %{count} characters is the maximum allowed"}
   validates :intercom_id, length: {maximum: 50, too_long: ": %{count} characters is the maximum allowed"}
   validates :intercom_id, :uniqueness => {:scope => :admin_id, message: ": You already have a site with that Intercom app id"}
